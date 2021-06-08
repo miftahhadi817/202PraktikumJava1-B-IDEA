2ca33aeec99f374beb6ce5eac55ab059b745e1d4
miftahhadi817 <ajago6028@gmail.com>
2021-06-08 18:48:35 +0100

    Memformat tanggal

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19710036";
         mahasiswa.nama = "Miftah Hadi";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("12-10-2000");
 
 
         System.out.println("Nama \t\t\t : " + mahasiswa.npm);
@@ -17,10 +23,12 @@ public class MahasiswaBeraksi {
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19638888";
         mahasiswa2.nama = "Delapu";
-        mahasiswa2.tanggalLahir = new Date();
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.npm);
         System.out.println("Npm \t\t\t : " + mahasiswa2.nama);
-        System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggalLahir);
+        System.out.println("Tanggal Lahir \t : " +
+                simpleDateFormat.format(mahasiswa2.tanggalLahir));
+
     }
 }
