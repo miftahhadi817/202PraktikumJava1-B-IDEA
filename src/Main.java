22062769902e5e4015f3b3b28f9c780c08ccfa0b
miftahhadi817 <ajago6028@gmail.com>
2021-06-08 17:13:13 +0100

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Miftah";
+        String namaTengah = "Hadi";
         String namaBelakang = "Hadi";
         int usia = 20;
         int targetTahunKuliah = 4;
@@ -11,5 +12,22 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan +" "+ namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap);
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(uniskaString.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,12));
+        System.out.println(namaLengkap.replace("Yog", "Barr"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split( " ");
+
+        for(String nama : namaArray)
+            System.out.println(nama);
     }
 }
